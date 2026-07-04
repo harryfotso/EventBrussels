3376cbc58c13b06c7c83fb7ec2e029a1f61714b7
Harry <harry.fotso.souopgui@ulb.be>
2026-07-04 21:52:12 +0200

    Initial Commit

---
+++ EventBrussels/EventBrusselsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventBrusselsApp.swift
+//  EventBrussels
+//
+//  Created by Harry on 04/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct EventBrusselsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
