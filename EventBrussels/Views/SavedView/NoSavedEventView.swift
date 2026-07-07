305ee59e81346621f1238a6cd321db59ba4361e3
Harry <fotsoharry@gmail.com>
2026-07-07 20:44:28 +0200

    Finish SavedEvent and NoSavedEvent + fix BottomBar

---
+++ EventBrussels/Views/SavedView/NoSavedEventView.swift
@@ -0,0 +1,19 @@
+//
+//  NoSavedEventView.swift
+//  EventBrussels
+//
+//  Created by Harry on 07/07/2026.
+//
+
+import SwiftUI
+
+struct NoSavedEventView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoSavedEventView()
+}
